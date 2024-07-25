ffee12238caec1e7b94ab0e682679fd773c5970f
checoman7 <sergio.gar.gonz@gmail.com>
2024-07-25 14:56:06 -0600

    mio

--- src/test/java/framework/Utils/BaseTest.java
+++ src/test/java/framework/Utils/BaseTest.java
@@ -2,6 +2,7 @@ package framework.Utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -12,7 +13,13 @@ public class BaseTest {
     protected CommonActions commands ;
     @BeforeMethod(groups = {"regression","smoke"})
     public void setUp(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Run in headless mode
+        options.addArguments("--disable-gpu"); // Applicable to Windows OS only
+        options.addArguments("--window-size=1920,1080"); // Specify window size to prevent issues with default size
+        options.addArguments("--no-sandbox"); // Required in some CI environments
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
