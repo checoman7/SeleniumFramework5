f7928c130e709e0281120f700a93c9681adf49d9
checoman7 <sergio.gar.gonz@gmail.com>
2024-07-25 14:58:13 -0600

    mio

--- src/test/java/framework/Utils/BaseTest.java
+++ src/test/java/framework/Utils/BaseTest.java
@@ -15,10 +15,10 @@ public class BaseTest {
     public void setUp(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless"); // Run in headless mode
-        options.addArguments("--disable-gpu"); // Applicable to Windows OS only
-        options.addArguments("--window-size=1920,1080"); // Specify window size to prevent issues with default size
-        options.addArguments("--no-sandbox"); // Required in some CI environments
-        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--disable-gpu"); // Applicable to Windows OS only
+//        options.addArguments("--window-size=1920,1080"); // Specify window size to prevent issues with default size
+//        options.addArguments("--no-sandbox"); // Required in some CI environments
+//        options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
