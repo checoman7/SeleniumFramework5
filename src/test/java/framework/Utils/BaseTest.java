80507c02cfa7d9d0a43153e6e5b421d2fa0f1409
checoman7 <sergio.gar.gonz@gmail.com>
2024-07-25 15:05:01 -0600

    mio

--- src/test/java/framework/Utils/BaseTest.java
+++ src/test/java/framework/Utils/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless"); // Run in headless mode
         options.addArguments("--window-size=1920,1080"); // Specify window size to prevent issues with default size
-        options.addArguments("--no-sandbox"); // Required in some CI environments
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
